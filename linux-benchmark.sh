e4bf99c370bda7c46787fb5f16a3e54d775fe782
westhecool <52859033+westhecool@users.noreply.github.com>
2023-09-10 12:02:17 -0400

    Update linux-benchmark.sh

--- linux-benchmark.sh
+++ linux-benchmark.sh
@@ -5,7 +5,7 @@ ENDCOLOR="\e[0m"
 echo -e "${GREEN}RUNNING RAM BENCHMARK${ENDCOLOR}"
 sysbench --verbosity=3 memory run
 num_cores=$(nproc)
-for ((cores=1; cores <= $num_cores; cores++)); do
+for ((cores=1; cores <= num_cores; cores++)); do
     echo -e "${GREEN}RUNNING CPU BENCHMARK (Cores: $cores)${ENDCOLOR}"
     sysbench --verbosity=3 --threads=$cores cpu run
 done
